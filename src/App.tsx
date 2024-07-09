45b4cfeefc58f78b1bd8e889a834d98cacb9848c
siddharth bhandari <siddharth.bhandari@gmail.com>
2024-07-09 23:47:46 +0530

    initial sample junk code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [i, setStart] = useState<boolean>(false);
+
+  const [counter, setCounter] = useState(0);
+  
+  useMemo(async () => {
+    console.log("called!!")
+    let t = new Promise((res) => setTimeout(res, .00000045));
+    await t;
+    if (i) {
+      setCounter(counter + 1);
+      return counter;
+    } else {
+      return counter;
+    }
+  }, [counter])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => { setStart(!i);
+          setCounter(counter + 1);
+         }}>
+         Start
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>{counter}</p>
+        <p>{i ? "true": "false"} -- help</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
